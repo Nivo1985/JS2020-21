c60a12a45c9025a9edd12c7314600d2ca917104a
Nivo1985 <karol.rogowski@gmail.com>
2020-11-05 11:23:18 +0100

    Add files via upload

---
+++ Sem1/Zjazd3/4)Features/1)Constants.js
@@ -0,0 +1,18 @@
+// const constVar = 2;
+// // const constVar2; //??
+// constVar = 3;
+// console.log(constVar);
+
+// const cArray = [1, 2, 3];
+// cArray = [3, 2, 1];
+
+// const cArray = [1, 2, 3];
+// cArray.push(4);
+// console.log(cArray);
+
+// const objC = { a: 1, b: 2, c: 3 };
+// objC = {};
+
+const objC = { a: 1, b: 2, c: 3 };
+objC.d = 4;
+console.log(objC);
---
+++ Sem1/Zjazd3/5)Functions/5)this.js
@@ -0,0 +1,56 @@
+//1
+
+// (function () {
+//   console.log(this);
+// })();
+
+//2
+// let id = 3;
+// let obj = {
+//   id: 1,
+//   getThisId: function () {
+//     let id = 2;
+//     return this.id;
+//   },
+//   getId: function () {
+//     let id = 2;
+//     return id;
+//   },
+//   getOuterId: function () {
+//     return id;
+//   },
+// };
+
+// console.log(obj.getThisId());
+// console.log(obj.getId());
+// console.log(obj.getOuterId());
+
+//3
+// let id = 2;
+
+// let obj = {
+//   id: 1,
+//   getId: function () {
+//     console.log(id);
+//   },
+// };
+
+// for (let i = 0; i < 1; i++) {
+//   let id = 3;
+//   obj.getId();
+// }
+
+// 4
+// let id = 2;
+
+// for (let i = 0; i < 1; i++) {
+//   let id = 3;
+
+//   let obj = {
+//     id: 1,
+//     getId: function () {
+//       console.log(id);
+//     },
+//   };
+//   obj.getId();
+// }
